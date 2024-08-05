441905d05dde15b0195db122a24174a93941b78d
csannhyp <csannhyp@users.noreply.github.com>
2024-08-05 08:09:54 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿     using BenchmarkDotNet.Attributes;
+     using BenchmarkDotNet.Running;
+     using NLog;
+     using Serilog;
+
+     public class LoggingBenchmark
+     {
+         private static readonly Logger NLogLogger = LogManager.GetCurrentClassLogger();
+         private static readonly Serilog.ILogger SerilogLogger = new LoggerConfiguration()
+             .WriteTo.Console()
+             .CreateLogger();
+
+         [Benchmark]
+         public void NLogBenchmark()
+         {
+             NLogLogger.Info("This is a test log message for NLog.");
+         }
+
+         [Benchmark]
+         public void SerilogBenchmark()
+         {
+             SerilogLogger.Information("This is a test log message for Serilog.");
+         }
+     }
+
+     public class Program
+     {
+         public static void Main(string[] args)
+         {
+             var summary = BenchmarkRunner.Run<LoggingBenchmark>();
+         }
+     }
+     
